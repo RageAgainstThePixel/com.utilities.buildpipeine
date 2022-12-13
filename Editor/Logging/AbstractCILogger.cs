ee67c24144a6fa8a23f9115a7f3c9965459c8204
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-13 00:22:23 -0500

    com.utilities.buildpipline 1.0.3 (#3)

--- Editor/Logging/AbstractCILogger.cs
+++ Editor/Logging/AbstractCILogger.cs
@@ -39,15 +39,15 @@ namespace Utilities.Editor.BuildPipeline.Logging
                 switch (logType)
                 {
                     case LogType.Log:
-                        format = $"\n{Log}{format}";
+                        format = $"\n{Log}{LogColor}{format}{ResetColor}";
                         break;
                     case LogType.Assert:
                     case LogType.Error:
                     case LogType.Exception:
-                        format = $"\n{Error}{format}";
+                        format = $"\n{Error}{ErrorColor}{format}{ResetColor}";
                         break;
                     case LogType.Warning:
-                        format = $"\n{Warning}{format}";
+                        format = $"\n{Warning}{WarningColor}{format}{ResetColor}";
                         break;
                     default:
                         throw new ArgumentOutOfRangeException(nameof(logType), logType, null);
@@ -72,9 +72,21 @@ namespace Utilities.Editor.BuildPipeline.Logging
         public virtual string Log => string.Empty;
 
         /// <inheritdoc />
-        public virtual string Error => string.Empty;
+        public virtual string LogColor => "\u001b[37m";
 
         /// <inheritdoc />
         public virtual string Warning => string.Empty;
+
+        /// <inheritdoc />
+        public virtual string WarningColor => "\u001b[33m";
+
+        /// <inheritdoc />
+        public virtual string Error => string.Empty;
+
+        /// <inheritdoc />
+        public virtual string ErrorColor => "\u001b[31m";
+
+        /// <inheritdoc />
+        public virtual string ResetColor => "\u001b[0m";
     }
 }
--- Editor/Logging/ICILogger.cs
+++ Editor/Logging/ICILogger.cs
@@ -15,13 +15,33 @@ namespace Utilities.Editor.BuildPipeline.Logging
         string Log { get; }
 
         /// <summary>
-        /// Error message prefix.
+        /// ANSI Log color code.
         /// </summary>
-        string Error { get; }
+        string LogColor { get; }
 
         /// <summary>
         /// Warning message prefix.
         /// </summary>
         string Warning { get; }
+
+        /// <summary>
+        /// ANSI Warning color code.
+        /// </summary>
+        string WarningColor { get; }
+
+        /// <summary>
+        /// Error message prefix.
+        /// </summary>
+        string Error { get; }
+
+        /// <summary>
+        /// ANSI Error color code.
+        /// </summary>
+        string ErrorColor { get; }
+
+        /// <summary>
+        /// ANSI Reset color code.
+        /// </summary>
+        string ResetColor { get; }
     }
 }
