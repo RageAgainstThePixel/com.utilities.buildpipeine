2c970e717273a3c43a912e52924196b581c4edda
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-08-20 23:29:12 -0400

    .

--- Utilities.BuildPipeline/Packages/com.utilities.buildpipeline/Editor/BuildInfo.cs
+++ Utilities.BuildPipeline/Packages/com.utilities.buildpipeline/Editor/BuildInfo.cs
@@ -118,7 +118,7 @@ namespace Utilities.Editor.BuildPipeline
                     case BuildTarget.StandaloneWindows:
                     case BuildTarget.StandaloneWindows64:
 #if PLATFORM_STANDALONE_WIN
-                        return UnityEditor.WindowsStandalone.UserBuildSettings.createSolution ? $"{Path.DirectorySeparatorChar}{Application.productName}.sln" : ".exe";
+                        return UnityEditor.WindowsStandalone.UserBuildSettings.createSolution ? $"{Path.DirectorySeparatorChar}{BundleIdentifier}" : ".exe";
 #else
                         return ".exe";
 #endif
