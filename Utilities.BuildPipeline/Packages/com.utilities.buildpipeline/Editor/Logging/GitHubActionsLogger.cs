833b38d2fb37751e51da71a9dad0b2a82cfd1a6b
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-05-09 09:24:47 -0400

    com.utilities.buildpipeline 1.7.0
    - fix GitHub Summary not showing up when build fails
    - added additional ignored logs
    - removed Build Outputs from GitHub Summary

--- Utilities.BuildPipeline/Packages/com.utilities.buildpipeline/Editor/Logging/GitHubActionsLogger.cs
+++ Utilities.BuildPipeline/Packages/com.utilities.buildpipeline/Editor/Logging/GitHubActionsLogger.cs
@@ -45,136 +45,125 @@ namespace Utilities.Editor.BuildPipeline.Logging
         {
             // temp disable logging to get the right messages sent.
             CILoggingUtility.LoggingEnabled = false;
-            var buildResultMessage = $"{buildReport.summary.platform} Build {buildReport.summary.result}!";
             var summary = Environment.GetEnvironmentVariable("GITHUB_STEP_SUMMARY");
             if (summary == null) { return; }
 
-            using (var summaryWriter = new StreamWriter(summary, true, Encoding.UTF8))
+            using var summaryWriter = new StreamWriter(summary, true, Encoding.UTF8);
+
+            var buildResultMessage = $"Unity {buildReport.summary.platform} Build {buildReport.summary.result}!";
+            summaryWriter.WriteLine($"# {buildResultMessage}");
+            summaryWriter.WriteLine("");
+
+            if (buildReport.summary.totalErrors > 0)
             {
-                summaryWriter.WriteLine($"# {buildResultMessage}");
-                summaryWriter.WriteLine("");
+                summaryWriter.WriteLine($"Errors: {buildReport.summary.totalErrors}");
+            }
 
-                if (buildReport.summary.totalErrors > 0)
+            if (buildReport.summary.totalWarnings > 0)
+            {
+                summaryWriter.WriteLine($"Warnings: {buildReport.summary.totalWarnings}");
+            }
+
+            summaryWriter.WriteLine($"Total duration: {stopwatch.Elapsed:g}");
+            summaryWriter.WriteLine($"Size: {FormatFileSize(buildReport.summary.totalSize)}");
+            summaryWriter.WriteLine($"Output Path: {buildReport.summary.outputPath}");
+            summaryWriter.WriteLine("");
+
+            var totalBuildTime = TimeSpan.Zero;
+            var logs = new List<string>();
+
+            foreach (var step in buildReport.steps)
+            {
+                totalBuildTime += step.duration;
+                var hasMessages = step.messages.Length > 0;
+
+                if (!hasMessages) { continue; }
+
+                var errorMessages = step.messages
+                    .Where(message => message.type is LogType.Error or LogType.Assert or LogType.Exception)
+                    .ToList();
+                var warningMessages = step.messages
+                    .Where(message => message.type == LogType.Warning)
+                    .ToList();
+                var logMessages = step.messages
+                    .Where(message => message.type == LogType.Log)
+                    .ToList();
+                var sortedMessages = new List<BuildStepMessage>();
+                sortedMessages.AddRange(errorMessages);
+                sortedMessages.AddRange(warningMessages);
+                sortedMessages.AddRange(logMessages);
+
+                foreach (var message in sortedMessages)
                 {
-                    summaryWriter.WriteLine($"Errors: {buildReport.summary.totalErrors}");
-                }
+                    var logMessage = message.content.Replace("\n", string.Empty);
+                    logMessage = logMessage.Replace("\r", string.Empty);
+                    logMessage = logMessage.Replace(Error, string.Empty);
+                    logMessage = logMessage.Replace(Warning, string.Empty);
+                    logMessage = logMessage.Replace(ErrorColor, string.Empty);
+                    logMessage = logMessage.Replace(WarningColor, string.Empty);
+                    logMessage = logMessage.Replace(ResetColor, string.Empty);
+                    logMessage = logMessage.Replace(LogColor, string.Empty);
 
-                if (buildReport.summary.totalWarnings > 0)
-                {
-                    summaryWriter.WriteLine($"Warnings: {buildReport.summary.totalWarnings}");
-                }
-
-                summaryWriter.WriteLine($"Total duration: {stopwatch.Elapsed:g}");
-                summaryWriter.WriteLine($"Size: {FormatFileSize(buildReport.summary.totalSize)}");
-                summaryWriter.WriteLine($"Output Path: {buildReport.summary.outputPath}");
-                summaryWriter.WriteLine("");
-
-                var totalBuildTime = TimeSpan.Zero;
-                var logs = new List<string>();
-
-                foreach (var step in buildReport.steps)
-                {
-                    totalBuildTime += step.duration;
-                    var hasMessages = step.messages.Length > 0;
-
-                    if (!hasMessages)
+                    switch (message.type)
                     {
-                        continue;
-                    }
-
-                    foreach (var message in step.messages)
-                    {
-                        var logMessage = message.content.Replace("\n", string.Empty);
-                        logMessage = logMessage.Replace("\r", string.Empty);
-                        logMessage = logMessage.Replace(Error, string.Empty);
-                        logMessage = logMessage.Replace(Warning, string.Empty);
-                        logMessage = logMessage.Replace(ErrorColor, string.Empty);
-                        logMessage = logMessage.Replace(WarningColor, string.Empty);
-                        logMessage = logMessage.Replace(ResetColor, string.Empty);
-                        logMessage = logMessage.Replace(LogColor, string.Empty);
-
-                        switch (message.type)
-                        {
-                            case LogType.Error:
-                            case LogType.Assert:
-                            case LogType.Exception:
-                                logs.Add($"| :boom: {message.type} | {logMessage} |");
-
-                                break;
-                            case LogType.Warning:
-                                logs.Add($"| :warning: {message.type} | {logMessage} |");
-
-                                break;
-                            case LogType.Log:
-                            default:
-                                logs.Add($"| {message.type} | {logMessage} |");
-
-                                break;
-                        }
+                        case LogType.Error:
+                        case LogType.Assert:
+                        case LogType.Exception:
+                            logs.Add($"| :boom: {message.type} | {logMessage} |");
+                            break;
+                        case LogType.Warning:
+                            logs.Add($"| :warning: {message.type} | {logMessage} |");
+                            break;
+                        case LogType.Log:
+                        default:
+                            logs.Add($"| {message.type} | {logMessage} |");
+                            break;
                     }
                 }
+            }
 
-                if (logs.Count > 0)
-                {
-                    var hasErrors = logs.Any(log => log.Contains($"| :boom: {LogType.Error} |"));
+            if (logs.Count > 0)
+            {
+                var hasErrors = logs.Any(log => log.Contains($"| :boom: {LogType.Error} |"));
 
-                    summaryWriter.WriteLine(hasErrors
-                        ? "<details open><summary>Logs</summary>"
-                        : "<details><summary>Logs</summary>");
+                summaryWriter.WriteLine(hasErrors
+                    ? "<details open><summary>Logs</summary>"
+                    : "<details><summary>Logs</summary>");
 
-                    summaryWriter.WriteLine("");
-                    summaryWriter.WriteLine("| log type | message |");
-                    summaryWriter.WriteLine("| -------- | ------- |");
-
-                    foreach (var log in logs)
-                    {
-                        summaryWriter.WriteLine(log);
-                    }
-
-                    summaryWriter.WriteLine("</details>");
-                }
-
-                summaryWriter.WriteLine("<details><summary>Build Outputs</summary>");
-                var fileList = new List<string>();
-#if UNITY_2022_1_OR_NEWER
-            fileList.AddRange(buildReport.GetFiles().Select(file => $"| {file.role} | {file.path} |"));
-#else
-                fileList.AddRange(buildReport.files.Select(file => $"{file.role} | {file.path}"));
-#endif
                 summaryWriter.WriteLine("");
-                summaryWriter.WriteLine("| file type | path |");
-                summaryWriter.WriteLine("| --------- | ---- |");
+                summaryWriter.WriteLine("| log type | message |");
+                summaryWriter.WriteLine("| -------- | ------- |");
 
-                foreach (var file in fileList)
+                foreach (var log in logs)
                 {
-                    summaryWriter.WriteLine(file);
+                    summaryWriter.WriteLine(log);
                 }
 
                 summaryWriter.WriteLine("</details>");
                 summaryWriter.WriteLine("");
-
-                switch (buildReport.summary.result)
-                {
-                    case BuildResult.Succeeded:
-                        Debug.Log($"{buildResultMessage}");
-
-                        break;
-                    case BuildResult.Unknown:
-                    case BuildResult.Cancelled:
-                        Debug.Log($"{WarningColor}{buildResultMessage}{ResetColor}");
-
-                        break;
-                    case BuildResult.Failed:
-                        Debug.Log($"{ErrorColor}{buildResultMessage}{ResetColor}");
-
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                summaryWriter.Close();
-                CILoggingUtility.LoggingEnabled = true;
             }
+
+            switch (buildReport.summary.result)
+            {
+                case BuildResult.Succeeded:
+                    Debug.Log($"{buildResultMessage}");
+
+                    break;
+                case BuildResult.Unknown:
+                case BuildResult.Cancelled:
+                    Debug.Log($"{WarningColor}{buildResultMessage}{ResetColor}");
+
+                    break;
+                case BuildResult.Failed:
+                    Debug.Log($"{ErrorColor}{buildResultMessage}{ResetColor}");
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            summaryWriter.Close();
+            CILoggingUtility.LoggingEnabled = true;
         }
     }
 }
